3e1ebf03d79dfed9792b8e9ffb3ca7446ec0d17f
EmeleCj <chijiokeemele@gmail.com>
2022-08-22 10:20:38 -0700

    A function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,7 +5,7 @@
  * @filename: filename.
  * @text_content: content writed in the file.
  *
- * Return: 1 if it success. -1 if it fails.
+ * Return: 1 if it successful. -1 if it fails.
  */
 int create_file(const char *filename, char *text_content)
 {
